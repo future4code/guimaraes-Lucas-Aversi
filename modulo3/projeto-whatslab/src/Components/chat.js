9031a86139927a320a73753a76eb5497a41e7367
Lucas Aversi <lucasaversi@gmail.com>
2022-02-05 00:49:03 -0300

    Projeto Whatslab

---
+++ modulo3/projeto-whatslab/src/Components/chat.js
@@ -0,0 +1,63 @@
+import React from 'react'
+
+class Chat extends React.Component{
+    state ={
+        chatmsg:  [],
+
+        inputNome: "",
+        inputMsg:""       
+    };
+
+    enviarMsg = () =>{
+        const novaMsg = {
+            nome: this.state.inputNome,
+            msg: this.state.inputMsg
+        };
+        
+        const addNovaMsg = [...this.state.chatmsg, novaMsg
+        ];
+
+        this.setState({chatmsg: addNovaMsg});   
+    };
+
+    onChangeAddNome = (e) => {
+        this.setState({inputNome: e.target.value});       
+    };
+
+    onChangeAddMsg = (e) => {
+        this.setState({inputMsg: e.target.value});
+    };
+    
+        render(){
+
+        const msgList = this.state.chatmsg.map((mensagem) =>{
+            return (
+                    <li>
+                        {mensagem.nome} - {mensagem.msg}
+                    </li>
+            );
+        });
+        return(
+            <div className='Chat'>
+                <div>
+                    <input
+                    value={this.state.inputNome}
+                    onChange={this.onChangeAddNome}
+                    placeholder={"Nome"}
+                    />
+
+                    <input
+                    value={this.state.inputMsg}
+                    onChange={this.onChangeAddMsg}
+                    placeholder={"Digite a sua mensagem"}
+                    />
+
+                    <button
+                     onClick={this.enviarMsg}>Enviar</button>      
+                </div>
+                <div className='Chat__Vertical'>{msgList}</div>
+            </div>
+        );
+   }
+}
+export default Chat
