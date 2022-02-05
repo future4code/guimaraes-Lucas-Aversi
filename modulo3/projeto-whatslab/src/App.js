807c51903e5b72e159cdb109136fd15cab76662d
Lucas Aversi <lucasaversi@gmail.com>
2022-02-04 23:47:22 -0300

    Projeto Whatslab

---
+++ modulo3/projeto-whatslab/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Chat  from './Components/chat.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
