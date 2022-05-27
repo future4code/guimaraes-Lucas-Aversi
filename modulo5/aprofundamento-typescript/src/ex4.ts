de17e5bd632983a7aa530e93865f266292b47216
Lucas Aversi <lucasaversi@gmail.com>
2022-05-26 22:29:30 -0300

    Aprofundamento TS - 26/05

---
+++ modulo5/aprofundamento-typescript/src/ex4.ts
@@ -0,0 +1,27 @@
+type pokemon = {
+	name: string,
+    types: string,
+	healthPoints: number
+}
+
+const pokemon1: pokemon = {
+  name: "Charmander",
+  types: "Fire",
+  healthPoints: 28
+}
+
+const pokemon2: pokemon = {
+  name: "Bulbasaur",
+  types: "Grass/Poison",
+
+  healthPoints: 31
+}
+
+const pokemon3: pokemon = {
+  name: "Squirtle",
+  types: "Water",
+  healthPoints: 35
+}
+
+
+console.log(pokemon3.healthPoints)
