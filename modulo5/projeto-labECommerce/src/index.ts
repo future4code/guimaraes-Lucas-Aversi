f9b81f2769005c96c9bd5177fd5379aff2e37db1
Lucas Aversi <lucasaversi@gmail.com>
2022-07-09 15:39:51 -0300

    CRIACAO DE ENDPOINT: GETALLPRODUCTS

--- modulo5/projeto-labECommerce/src/index.ts
+++ modulo5/projeto-labECommerce/src/index.ts
@@ -4,7 +4,9 @@ import { Request, Response } from "express"
 import { getAllUsers} from "./endpoints/getAllUsers";
 import { createNewUser } from "./endpoints/createNewUser";
 import { createNewProduct } from "./endpoints/createNewProduct";
+import { getAllProducts } from "./endpoints/getAllProducts";
 
 app.get("/users", getAllUsers)
 app.post("/users", createNewUser)
 app.post("/products", createNewProduct)
+app.get("/products", getAllProducts )
