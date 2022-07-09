7c2f9a1fe99e22713546411da7bc0c2e4d8ac567
Lucas Aversi <lucasaversi@gmail.com>
2022-07-09 15:28:49 -0300

    CRIACAO DE ENDPOINT: CREATENEWPRODUCT

--- modulo5/projeto-labECommerce/src/index.ts
+++ modulo5/projeto-labECommerce/src/index.ts
@@ -3,6 +3,8 @@ import {connection} from "./data/connection"
 import { Request, Response } from "express"
 import { getAllUsers} from "./endpoints/getAllUsers";
 import { createNewUser } from "./endpoints/createNewUser";
+import { createNewProduct } from "./endpoints/createNewProduct";
 
 app.get("/users", getAllUsers)
 app.post("/users", createNewUser)
+app.post("/products", createNewProduct)
