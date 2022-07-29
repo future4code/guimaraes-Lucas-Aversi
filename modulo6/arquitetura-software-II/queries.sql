47f7a3b6dbf49a3c06d62ebf030ebd7f886b2c01
Lucas Aversi <lucasaversi@gmail.com>
2022-07-28 22:44:51 -0300

    ARQUITETURA DE SOFTWARE II - 28/07/22

---
+++ modulo6/arquitetura-software-II/queries.sql
@@ -0,0 +1,15 @@
+-- Active: 1659048158253@@35.226.146.116@3306@guimaraes-4211386-lucas-aversi
+CREATE TABLE LABEFLIX_USER (
+	id VARCHAR(255) PRIMARY KEY, 
+    name VARCHAR(255) NULL, 
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE LABEFLIX_MOVIE (
+	id VARCHAR(255) PRIMARY KEY, 
+    title VARCHAR(255) NOT NULL, 
+    description TEXT NOT NULL,
+    duration_in_minutes INT NOT NULL, 
+    year_of_release INT NOT NULL
+);
